a397ab1235b6a15e59e8fce7fd5dac1cd5b42c35
Hoc Pham <phamthaihoc008@gmail.com>
2024-11-19 01:47:38 +0700

    Reapply "SetUp and seed data for Category use EF core,Category CRUD operation, Boostrap theme, ClientSide Validation, ServerSideValidation, TempData, Parital Views and Toastr Notification."
    This reverts commit 79bedf4d3cf58ab1f3699a79cbf5a1cc166faeb5.

--- BookSellWeb/Program.cs
+++ BookSellWeb/Program.cs
@@ -1,3 +1,6 @@
+using BookSellWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace BookSellWeb
 {
     public class Program
@@ -8,6 +11,9 @@ namespace BookSellWeb
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"))
+            );
 
             var app = builder.Build();
 

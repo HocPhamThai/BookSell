fad6e42171a873a39fc95654babd56ebc8f5c57e
Hoc Pham <phamthaihoc008@gmail.com>
2024-12-20 06:23:42 +0700

    Implement home page and detail page for books

--- BookSellWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookSellWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using BookEcomWeb.DataAccess.IRepository;
 using BookEcomWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,15 +8,24 @@ namespace BookEcomWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(inCludeProperties: "Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int productID)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.Id == productID, inCludeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()

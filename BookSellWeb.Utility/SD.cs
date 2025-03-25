28ce13f764920f4dd6d9e9b040a285f3cbd69698
hocpham <phamthaihoc008@gmail.com>
2025-03-25 22:37:42 +0700

    Modeule 11 - Step 3: Loading Summary UI with Data and Add Order Status

--- BookSellWeb.Utility/SD.cs
+++ BookSellWeb.Utility/SD.cs
@@ -6,5 +6,15 @@
         public const string Role_Company = "Company";
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
+
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProcess = "InProcess";
+        public const string StatusShipped = "Shipped";
+      
+        public const string PaymentStatusPending = "Pending";
+        public const string PamymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayPayment";
+        public const string PaymentStatusRejected = "Rejected";
     }
 }

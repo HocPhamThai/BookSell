4f9de38d8dc17f5a6690aa510c25a94a2e2690de
hocpham <phamthaihoc008@gmail.com>
2025-03-30 02:39:13 +0700

    Module 12: Step 2 - OrderVM, OrderController, Order List UI, Add Status Filter

---
+++ BookSellWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using BookEcomWeb.DataAccess.IRepository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookEcomWeb.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var objOrderHeaders = _unitOfWork.OrderHeader.GetAll(inCludeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeaders });
+        }
+        #endregion
+    }
+}

0980447a36a4ed48dfc4bbb6ffffd0d0e616c199
hocpham <phamthaihoc008@gmail.com>
2025-04-05 00:30:41 +0700

    Module 13 - step 3 - View Component in Action

--- BookSellWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookSellWeb/Areas/Customer/Controllers/HomeController.cs
@@ -23,20 +23,7 @@ namespace BookEcomWeb.Areas.Customer.Controllers
         }
 
         public IActionResult Index()
-        {
-            var claimIdentity = User.Identity as ClaimsIdentity;
-            Claim? claim = claimIdentity?.Claims.FirstOrDefault();
-
-            if (claim != null)
-            {
-                var count = _unitOfWork.ShoppingCart.GetAll(x => x.ApplicationUserId == claim.Value).Count();
-                HttpContext.Session.SetInt32(SD.SessionCart, count);
-            }
-            else
-            {
-                HttpContext.Session.SetInt32(SD.SessionCart, 0);
-            }
-            
+        {   
             IEnumerable<Product> productList = _unitOfWork.Product.GetAll(inCludeProperties: "Category");
             return View(productList);
         }
---
+++ BookSellWeb/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using BookEcomWeb.DataAccess.IRepository;
+using BookEcomWeb.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BookEcomWeb.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimIdentity = User.Identity as ClaimsIdentity;
+            Claim? claim = claimIdentity?.Claims.FirstOrDefault();
+            
+            if (claim != null)
+            {
+                if (HttpContext.Session.GetInt32(SD.SessionCart) == null)
+                {
+                    var count = _unitOfWork.ShoppingCart.GetAll(x => x.ApplicationUserId == claim.Value).Count();
+                    HttpContext.Session.SetInt32(SD.SessionCart, count);    
+                }
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
+    }
+}
